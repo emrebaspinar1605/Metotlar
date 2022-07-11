1424921e15c51a9fa07212367b1ce1f4690406b1
emrebaspinar1605 <turkuz-elhamdulillah@hotmail.com>
2022-07-11 14:50:57 +0300

    sss

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a=2,b=3;
+string yazi="Deneme";
+int sonuc=Topla(a,b);
+
+    System.Console.WriteLine(a+b);
+    System.Console.WriteLine(sonuc);
+
+Metotlar ornek= new Metotlar();
+    ornek.EkranaYazdir(yazi);
+
+static int Topla(int deger1,int deger2)
+{
+    return deger1+deger2;
+}
+
+class Metotlar
+{
+    public void EkranaYazdir(string veri)
+    {
+        System.Console.WriteLine(veri);
+    }
+}
